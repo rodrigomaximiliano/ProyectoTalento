f2c8911e6ea7c53734f30ae6d7367cae756fd94b
SergioAyala <sergiodark.sa@gmail.com>
2024-05-19 16:03:38 -0300

    Agregar productos al carrito

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,28 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+*/
+
+Route::get('/', [App\Http\Controllers\FrontController::class, 'index']);
+
+Route::post('cart/add', [App\Http\Controllers\CartController::class, 'add'])->name('add');
+Route::get('cart/chekout', [App\Http\Controllers\CartController::class, 'chekout'])->name('chekout');
+Route::get('cart/clear', [App\Http\Controllers\CartController::class, 'clear'])->name('clear');
+Route::post('cart/removeitem', [App\Http\Controllers\CartController::class, 'removeItem'])->name('removeitem');
+
 
 Auth::routes();
 
